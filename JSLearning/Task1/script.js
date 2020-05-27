9878852a6d44e81ddbd1e8c7aa2e668604ca7e45
MaksimBezdrobnoi <oculus.ee@gmail.com>
2020-05-28 01:18:47 +0300

    Add Exam for University

--- JSLearning/Task1/script.js
+++ JSLearning/Task1/script.js
@@ -1,6 +1,15 @@
 let money, time;
-money = prompt('Ваш бюджет на месяц?');
-time = prompt('Введите дату в формате YYYY-MM-DD');
+
+function start() {
+    money = +prompt("Ваш бюджет на месяц?", "");
+    time = prompt("Введите дату в формате YYYY-MM-DD", "");
+
+    while (isNaN(money) || money == "" || money == null) {
+        money = +prompt("Ваш бюджет на месяц?", "");
+    }
+};
+
+start();
 
 let appData = {
     budget: money,
@@ -8,15 +17,64 @@ let appData = {
     expenses: {},
     optionalExpenses: {},
     income: [],
-    savings: false
+    savings: true,
 };
 
-let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-    a2 = prompt("Во сколько обойдется?", ''),
-    a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-    a4 = prompt("Во сколько обойдется?", '');
+function choseExpenses() {
+    for (let i = 0; i < 2; i++) {
+        let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+            b = prompt("Во сколько обойдется?", "");
 
-appData.expenses[a1] = a2;
-appData.expenses[a3] = a4;
+        if (
+            typeof a != null &&
+            typeof b != null &&
+            a != "" &&
+            b != "" &&
+            a.length < 50
+        ) {
+            console.log("done");
+            appData.expenses[a] = b;
+        } else {}
+    }
+}
+choseExpenses();
 
-alert(appData.budget / 30);
+
+function detectDayBudget() { // Расчет дневного бюджета
+    appData.moneyPerDay = (appData.budget / 30).toFixed();
+    alert("Бюджет на 1 день составляет " + appData.moneyPerDay + "руб.");
+}
+detectDayBudget();
+
+function detectLevel() {
+    if (appData.moneyPerDay < 100) {
+        console.log("Минимальный уровень достатка");
+    } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+        console.log("Средний уровень достатка");
+    } else if (appData.moneyPerDay > 2000) {
+        console.log("Высокий уровень достатка");
+    }
+}
+
+detectLevel();
+
+
+
+function checkSavings() {
+    if (appData.savings == true) {
+        let save = +prompt("Какова сумма накоплений?"),
+            percent = +prompt("Под какой процент?");
+
+        appData.monthIncome = save / 100 / 12 * percent;
+        alert("Доход в месяц с вашего депозита: " + appData.monthIncome);
+    }
+}
+
+checkSavings();
+
+function chooseOptExpenses() {
+    for (let i = 1; i < 4; i++) {
+        let a = prompt("Статья необязательных расходов?")
+        appData.optionalExpenses[i] = a;
+    }
+};
---
+++ University/web technology/examyii/views/site/error.php
@@ -0,0 +1,26 @@
+<?php
+
+/* @var $this yii\web\View */
+/* @var $name string */
+/* @var $message string */
+/* @var $exception Exception */
+
+use yii\helpers\Html;
+
+$this->title = $name;
+?>
+
+
+<div class="banner">
+    <?= $this->render('//layouts/inc/sidebar') ?>
+    <div class="w3l_banner_nav_right">
+        <div style="padding: 0 1em 1em ">
+            <h2><?= Html::encode($this->title) ?></h2>
+            <div class="alert alert-danger">
+                <?= nl2br(Html::encode($message)) ?>
+            </div>
+        </div>
+    </div>
+    <div class="clearfix"></div>
+</div>
+
